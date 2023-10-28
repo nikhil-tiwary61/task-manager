ce3b1c996e52fda72b5312553afb300326411245
Nikhil Tiwary <ntiwary61@gmail.com>
2023-10-28 17:46:29 +0530

    feat: add route and controller for getAllTasks api

---
+++ controllers/tasks.js
@@ -0,0 +1,7 @@
+const getAllTasks = (req, res) => {
+  res.send("get all items");
+};
+
+module.exports = {
+  getAllTasks,
+};
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const tasks = require("./routes/tasks");
 const app = express();
-const port = 8080;
 
+//middlewares
+app.use(express.json());
+
+//routes
+app.get("/hello", (req, res) => {
+  res.send("hello world");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 8080;
 app.listen(port, () => {
   console.log("server started");
 });
