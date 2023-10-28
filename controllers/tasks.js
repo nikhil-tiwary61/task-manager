8a7e9299f04336036c9fbb277c40580a01f7cf25
Nikhil Tiwary <ntiwary61@gmail.com>
2023-10-28 19:23:10 +0530

    feat: add controllers for CRUD operations

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,28 @@
 const getAllTasks = (req, res) => {
-  res.send("get all items");
+  res.send("get all tasks");
+  res.sendStatus(200);
+};
+const getTask = (req, res) => {
+  res.send("get task");
+  res.sendStatus(200);
+};
+const createTask = (req, res) => {
+  res.send("task created");
+  res.sendStatus(201);
+};
+const updateTask = (req, res) => {
+  res.send("task updated");
+  res.sendStatus(200);
+};
+const deleteTask = (req, res) => {
+  res.send("task deleted");
+  res.sendStatus(200);
 };
 
 module.exports = {
   getAllTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
 };
