35bb997a95301e4f3f8b6138ee76a5106cb1981f
Nikhil Tiwary <ntiwary61@gmail.com>
2023-11-01 23:22:02 +0530

    feat: connect application to mongodb database

--- db/connect.js
+++ db/connect.js
@@ -1,2 +1,11 @@
+const mongoose = require("mongoose");
+
 const connectionString =
-  "mongodb+srv://nikhiltiwary28022000:<password>@cluster0.h0pfgvc.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://nikhiltiwary28022000:yUxoaMkK4BKqsS4K@cluster0.h0pfgvc.mongodb.net/task-manager?retryWrites=true&w=majority";
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(connectionString);
+  console.log("database connected");
+}
