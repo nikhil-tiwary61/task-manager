73d9c1641b1089b485fd4224dbf85998861d82fd
Nikhil Tiwary <ntiwary61@gmail.com>
2023-12-14 11:58:31 +0530

    change createTask controller to create a task in our database with error handling and changes in schema

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,3 +1,5 @@
+const Task = require("../models/tasks");
+
 const getAllTasks = (req, res) => {
   res.send("get all tasks");
   res.sendStatus(200);
@@ -6,9 +8,13 @@ const getTask = (req, res) => {
   res.json({ id: req.params.id });
   res.sendStatus(200);
 };
-const createTask = (req, res) => {
-  res.json(req.body);
-  res.sendStatus(201);
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 const updateTask = (req, res) => {
   res.send("task updated");
